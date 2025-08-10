899e0e3a2d59bc5fe70772b72de8a990f4e40a71
Sam20B <wafulabibiana93@gmail.com>
2025-08-10 17:43:51 +0300

    typo

--- Sam20B/linked_list.py
+++ Sam20B/linked_list.py
@@ -3,12 +3,12 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
-        
+
 class LinkedList:
-    
+
     def __init__(self):
         self.head = None # Initialize an empty linked list
-    
+
     def append(self, value):
         """Adds a new node to the end of the linked list."""
         new_node = Node(value)
@@ -19,7 +19,7 @@ class LinkedList:
             while current.next:
                 current = current.next
             current.next = new_node
-            
+
     def display(self, message=""):
         print(message, end="") # Print the message inputed and stay on the same line
         current = self.head
@@ -30,18 +30,18 @@ class LinkedList:
                 print(current.value, end=" -> ")
             current = current.next
         print() # New line after printing the list
-        
+
     def reverse(self):
         prev = None
         current = self.head
-        
+
         while current:
             next_node = current.next # Store the next node in memory
             current.next = prev # Reverse the pointer
             prev = current # Move prev to current
             current = next_node # Move to the next node
         self.head = prev # Update the last node as the head
-        
+
 # Create the linked list
 li = LinkedList()
 
@@ -52,7 +52,7 @@ li.append(4)
 li.append(5)
 
 #Display the original list
-li.display("Linkedn List: ") #Linked List: 1 -> 2 -> 3 -> 4 -> 5
+li.display("Linked List: ") #Linked List: 1 -> 2 -> 3 -> 4 -> 5
 
 li.reverse() # Reverse the list
 
